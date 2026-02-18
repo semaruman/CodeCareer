3b90d47c4c7fa032aa421f4299c5db24c4304098
Semen <semen.ruman@gmail.com>
2026-02-18 16:53:33 +0300

    Добавил модель постов(публикаций)

---
+++ CodeCareer/Areas/User/Models/PublicationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CodeCareer.Areas.User.Models
+{
+    public class PublicationModel
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите описание")]
+        [MaxLength(1000, ErrorMessage = "Максимальная длина - 1000 символов")]
+        public string Content {  get; set; }
+
+    }
+}
