ed9f7a65c25381c2efc6ef101f11810982b95edb
Semen <semen.ruman@gmail.com>
2026-02-22 11:30:44 +0300

    Добавил авторизацию в панель администратора

--- CodeCareer/Areas/Admin/Controllers/HomeController.cs
+++ CodeCareer/Areas/Admin/Controllers/HomeController.cs
@@ -1,11 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeCareer.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodeCareer.Areas.Admin.Controllers
 {
+    [Route("{area}")]
+    [Route("{area}/{action}")]
     public class HomeController : Controller
     {
+        public static AdminModel admin = new AdminModel();
+
+        [Route("")]
         public IActionResult Index()
         {
+            if (!admin.IsAuthorizate)
+            {
+                return RedirectToAction("Authorization");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Authorization()
+        {
+            ViewBag.WrongPassword = string.Empty;
+            return View(admin);
+        }
+
+        [HttpPost]
+        public IActionResult Authorization(AdminModel adminP)
+        {
+            if (adminP.Password == AdminModel.PASSWORD)
+            {
+                admin = adminP;
+                admin.IsAuthorizate = true;
+                return RedirectToAction("AuthorizationSuccess");
+            }
+            else
+            {
+                ViewBag.WrongPassword = "Неверный пароль!";
+                return View(admin);
+            }
+        }
+
+        public IActionResult AuthorizationSuccess()
+        {
+            return View();
+        }
+
+        public IActionResult LogoutAdmin()
+        {
+            admin = new AdminModel();
             return View();
         }
     }
---
+++ CodeCareer/Areas/Admin/Models/AdminModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeCareer.Areas.Admin.Models
+{
+    public class AdminModel
+    {
+        public static string PASSWORD = "adminPassword";
+
+        public string Password { get; set; } = string.Empty;
+        public bool IsAuthorizate { get; set;} = false;
+    }
+}
