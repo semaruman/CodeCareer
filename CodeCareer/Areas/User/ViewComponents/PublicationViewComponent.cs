32652facb2d30d8d22d4371a72ebc60adf52c928
Semen <semen.ruman@gmail.com>
2026-02-25 06:44:49 +0300

    Добавил ViewComponent для отображения публикации. Применил его в ленте

---
+++ CodeCareer/Areas/User/ViewComponents/PublicationViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using CodeCareer.Areas.User.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCareer.Areas.User.ViewComponents
+{
+    public class PublicationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PublicationModel publicationP)
+        {
+            return View(publicationP);
+        }
+    }
+}
