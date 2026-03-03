95d6e4ad664f71109fc927b6268048e7cdc9066d
Semen <semen.ruman@gmail.com>
2026-03-03 07:34:53 +0300

    Добавил контроллер для решения задачи. Сделал главную страницу и настроил маршрутизацию

---
+++ CodeCareer/Areas/User/Controllers/TasksSolvingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCareer.Areas.User.Controllers
+{
+    [Area("User")]
+    public class TasksSolvingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult OlympSolving()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FrontendSolving()
+        {
+            return View();
+        }
+    }
+}
