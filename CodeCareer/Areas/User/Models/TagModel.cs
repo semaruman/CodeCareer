c9c9079d8044d86f7cfe56a60bd92adca9cd3f86
Semen <semen.ruman@gmail.com>
2026-03-18 06:26:31 +0300

    Добавил Data Annotations к моделям Tag и Task для работы с EF core

--- CodeCareer/Areas/User/Models/TagModel.cs
+++ CodeCareer/Areas/User/Models/TagModel.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CodeCareer.Areas.User.Models
 {
     public class TagModel
     {
         public int Id { get; set; }
+
+        [Column("name")]
         public string Name {  get; set; }
 
+        [Column("img_path")]
         public string ImgPath { get; set; }
 
         public TagModel()
--- CodeCareer/Areas/User/Models/TaskModel.cs
+++ CodeCareer/Areas/User/Models/TaskModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
@@ -10,18 +11,23 @@ namespace CodeCareer.Areas.User.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Введите название задачи")]
+        [Column("name")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Выберите тип задачи")]
+        [Column("type")]
         public string Type {get;set;}
 
         [Required(ErrorMessage = "Введите содержимое задачи")]
+        [Column("content")]
         public string Content { get; set; }
 
         [Required(ErrorMessage = "Опишите входные данные задачи")]
+        [Column("input_content")]
         public string InputContent { get; set; }
 
         [Required(ErrorMessage = "Опишите выходные данные задачи")]
+        [Column("output_content")]
         public string OutputContent { get; set; }
 
 
