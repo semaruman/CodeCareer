a086bce04a536309541d6a6e647cf109b7ead4c2
Semen <semen.ruman@gmail.com>
2026-03-18 06:34:58 +0300

    Добавил класс TagMySqlEfService для взаимодействия с таблицей тегов через EF core

--- CodeCareer/Areas/User/Services/Implementations/MySqlEfServices/TagMySqlEfService.cs
+++ CodeCareer/Areas/User/Services/Implementations/MySqlEfServices/TagMySqlEfService.cs
@@ -1,7 +1,31 @@
-﻿namespace CodeCareer.Areas.User.Services.Implementations.MySqlEfServices
-{
-    public class TagMySqlEfService
-    {
+﻿using CodeCareer.Areas.User.Data;
+using CodeCareer.Areas.User.Models;
+using CodeCareer.Areas.User.Services.Interfaces;
 
+namespace CodeCareer.Areas.User.Services.Implementations.MySqlEfServices
+{
+    public class TagMySqlEfService : ITagService
+    {
+        public List<TagModel> GetTagModels()
+        {
+            using var dbContext = new ApplicationDbContext();
+
+            return dbContext.Tags.ToList();
+        }
+
+        public void AddTagModel(TagModel tag)
+        {
+            using var dbContext = new ApplicationDbContext();
+            dbContext.Tags.Add(tag);
+            dbContext.SaveChanges();
+        }
+
+        public void RemoveTagModel(string tagName)
+        {
+            using var dbContext = new ApplicationDbContext();
+            var dbTag = dbContext.Tags.FirstOrDefault(t => t.Name == tagName);
+            dbContext.Tags.Remove(dbTag);
+            dbContext.SaveChanges();
+        }
     }
 }
