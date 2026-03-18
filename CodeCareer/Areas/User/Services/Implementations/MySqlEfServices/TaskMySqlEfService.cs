20c870e62962f60aec8172172f13726008ad2773
Semen <semen.ruman@gmail.com>
2026-03-18 06:48:32 +0300

    Добавил TaskMySqlEfService для работы с таблицей задач базы данных через EF core

---
+++ CodeCareer/Areas/User/Services/Implementations/MySqlEfServices/TaskMySqlEfService.cs
@@ -0,0 +1,30 @@
+﻿using CodeCareer.Areas.User.Data;
+using CodeCareer.Areas.User.Models;
+using CodeCareer.Areas.User.Services.Interfaces;
+
+namespace CodeCareer.Areas.User.Services.Implementations.MySqlEfServices
+{
+    public class TaskMySqlEfService : ITaskService
+    {
+        public List<TaskModel> GetTaskModels()
+        {
+            using var dbContext = new ApplicationDbContext();
+            return dbContext.Tasks.ToList();
+        }
+
+        public void AddTaskModel(TaskModel task)
+        {
+            using var dbContext = new ApplicationDbContext();
+            dbContext.Tasks.Add(task);
+            dbContext.SaveChanges();
+        }
+
+        public void RemoveTaskModel(int id)
+        {
+            using var dbContext = new ApplicationDbContext();
+            var task = dbContext.Tasks.Find(id);
+            dbContext.Tasks.Remove(task);
+            dbContext.SaveChanges();
+        }
+    }
+}
--- CodeCareer/Areas/User/Services/Interfaces/ITaskService.cs
+++ CodeCareer/Areas/User/Services/Interfaces/ITaskService.cs
@@ -5,7 +5,7 @@ namespace CodeCareer.Areas.User.Services.Interfaces
     public interface ITaskService
     {
         public List<TaskModel> GetTaskModels();
-        public void AddTaskModel(TaskModel tag);
+        public void AddTaskModel(TaskModel task);
         public void RemoveTaskModel(int id);
     }
 }
